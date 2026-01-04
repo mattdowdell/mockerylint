478b56eabc70324adcd55263b6e7465d78e877e5
Matt Dowdell <mdowdell244@gmail.com>
2026-01-04 11:05:32 +0000

    Add more test cases

--- testdata/usefactory/usefactory_test.go
+++ testdata/usefactory/usefactory_test.go
@@ -23,3 +23,17 @@ func TestExample_Bad2(t *testing.T) {
 
 	mock.AssertExpectations(t) // want `\.AssertExpectations\(\) can be removed when using mock factory`
 }
+
+func TestExample_Bad3(t *testing.T) {
+	mock := NewMockExample(t)
+	mock.Test(t) // want `\.Test\(\) can be removed when using mock factory`
+
+	mock.AssertExpectations(t) // want `\.AssertExpectations\(\) can be removed when using mock factory`
+}
+
+func TestExample_Bad4(t *testing.T) {
+	mock := NewMockExample(t)
+	mock.Mock.Test(t) // want `\.Test\(\) can be removed when using mock factory`
+
+	mock.Mock.AssertExpectations(t) // want `\.AssertExpectations\(\) can be removed when using mock factory`
+}
