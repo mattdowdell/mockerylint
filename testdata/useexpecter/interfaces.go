378ca584050287dc2f5fd46783f5380755d5fef2
Matthew Dowdell <mattdowdell@users.noreply.github.com>
2026-08-27 11:20:42 +0100

    fix: Check with-expecter was enabled in useexpecter rule (#113)
    ## Description
    
    While the latest versions of `mockery` have `with-expecter` enabled
    unconditionally, it was opt-in for v2. As such, there exist generated
    mocks that should not have expectations subject to the `useexpecter`
    rule.
    
    Fixes #47.
    
    ## Changes
    
    - Skip mock expectations where the mock has no `.EXPECT()` method.

--- testdata/useexpecter/interfaces.go
+++ testdata/useexpecter/interfaces.go
@@ -3,3 +3,8 @@ package useexpecter
 type Example interface {
 	Example(int) int
 }
+
+// Legacy is mocked without with-expecter enabled, as Mockery allowed before v3.0.0.
+type Legacy interface {
+	Example(int) int
+}
